57726ec768bfc078155795cfe056a5ac31cbfacd
Jordy Awas <jordankhola@gmail.com>
2021-09-22 13:36:08 -0400

    revision

---
+++ app.java/file.sh
@@ -0,0 +1,5 @@
+#lets create a file
+echo "what is the file name"
+read filename
+echo The filename is $filename
+touch $filename
---
+++ app.java/myapp.java
@@ -0,0 +1,4 @@
+echo "what is your name"
+read name
+echo "my name is $name"
+echo "Welcome to Landmark $name"
